86f89196f61a83d5b72a0d8dc7203e101152db53
HarLem <phanhieulam1101@gmail.com>
2023-12-23 13:19:25 +0700

    sua lai form sua tai len anh

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -8,14 +8,14 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Bảng điều khiển</h1>
+                    <h1 class="m-0">Bảng Điều Khiển</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item">
                             <a href="<?=ADMIN_URL?>/dasboard/"><i class="fas fa-home"></i></a>
                         </li>
-                        <li class="breadcrumb-item active">Bảng Điều Khiển</li>
+                        <li class="breadcrumb-item active">Bảng điều khiển</li>
                     </ol>
                 </div>
             </div>
